0b9f011ff8e21b31a00431f0f3f4f220ca3ef853
John Westhoff <jwesthof@nd.edu>
2016-12-08 15:41:23 -0500

    Updated for running on johnbot.me
    Includes changing path, running the command, and allowing stuff.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 var express = require('express')
 var exec = require('child_process').exec;
 var app = express()
-    
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.get('/snow', function(req, res) {
           // here we would call the dijkstras program and pipe in my location.  assuming the form: startx,starty,endx,endy
           var list = req.query.loc.split(",");
@@ -9,11 +15,11 @@ var app = express()
           for( var i in list){
               strold += list[i] + " ";
           }
-          strold += "' | ../shortestpath/dijkstras ";
+          strold += "' | ./shortestpath/dijkstras ";
           console.log(strold);
-          exec('pwd', function callback(error, stdout, stderr){
+          exec(strold, function callback(error, stdout, stderr){
               res.json({path: stdout, queries:list})
           });
     })
- 
+  
 app.listen(3000)
